6b158291e7ba82f07c88e342554ccbcd8048f2bd
Peter <pgkutc@gmail.com>
2021-01-09 16:42:54 -0500

    created message component

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Chat.css';
 import {useParams} from 'react-router-dom';
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined"
@@ -7,13 +7,31 @@ import db from './firebase';
 
 function Chat() {
     const {channelId} = useParams();
+    const [channelDetails, setChannelDetails] = useState(null);
+    const [channelMessages, setChannelMessages] = useState([]);
+
+    useEffect(() => {
+        if (channelId) {
+            db.collection('channels')
+                .doc(channelId)
+                .onSnapshot(snapshot => (
+                    setChannelDetails(snapshot.data())))
+        }
+
+        db.collection("channels")
+            .doc(channelId)
+            .collection('messages')
+            .orderBy('timestamp',"asc")
+            .onSnapshot((snapshot) => setChannelMessages(snapshot.docs.map((doc) => doc.data())))
+
+    }, [channelId])
 
     return (
         <div className="chat">
             <div className="chat__header">
                 <div className="chat__headerLeft">
                     <h4 className="chat__channelName">
-                        <strong>#General</strong>
+                        <strong>#{channelDetails?.name}</strong>
                         <StarBorderOutlinedIcon/>
                     </h4>
                 </div>
@@ -22,6 +40,16 @@ function Chat() {
                         <InfoOutlinedIcon/> Details
                     </p>
                 </div>
+            </div>
+            <div className="chat__messages">
+                {channelMessages.map(({message, timestamp, user, userImage}) => (
+                    <Message
+                        message={message}
+                        timestamp ={timestamp}
+                        user ={user}
+                        userImage = {userImage}
+                    />
+                ))}
             </div>
         </div>
     );
