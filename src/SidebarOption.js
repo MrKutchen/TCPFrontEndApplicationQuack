68ac1e491a720bcf5418de4b88c97ee9f62ed68c
Peter <pgkutc@gmail.com>
2021-01-09 16:16:17 -0500

    can add channels

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import './SidebarOption.css';
+import {useHistory} from 'react-router-dom';
+import db from './firebase';
 
 function SidebarOption({Icon, title, id, addChannelOption}) {
-    // const history = useHistory();
-    //
-    // const selectChannel = () => {
-    //     if (id) {
-    //         if (id) {
-    //             history.push(`/room/${id}`);
-    //         } else {
-    //             history.push(title);
-    //         }
-    //     }
-    // };
-    //
-    // const addChannel = () => {
-    //     const channelName = prompt('Please enter the channel name');
-    //
-    //     if(channelName) {
-    //         db.collection('rooms').add({
-    //             name:channelName
-    //         })
-    //     }
-    // };
+    const history = useHistory();
+
+    const selectChannel = () => {
+        if (id) {
+            if (id) {
+                history.push(`/channel/${id}`);
+            } else {
+                history.push(title);
+            }
+        }
+    };
+
+    const addChannel = () => {
+        const channelName = prompt('Please enter the new channel name');
+
+        if (channelName) {
+            db.collection('channels').add({
+                name: channelName
+            })
+        }
+    };
 
     return (
         <div className="sidebarOption"
-            // onClick={addChannelOption ? addChannel : selectChannel}
+             onClick={addChannelOption ? addChannel : selectChannel}
         >
             {Icon && <Icon className="sidebarOption__icon"/>}
             {Icon ? (<h3>
